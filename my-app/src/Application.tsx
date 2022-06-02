b890457ca67ef70864d6852ea80658eac961a997
Billytckwong <tckwong@shoalter.com>
2022-06-02 16:25:05 +0800

    can see data change

---
+++ my-app/src/Application.tsx
@@ -0,0 +1,55 @@
+import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import './Application.css';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import Widgets from './components/Widgets';
+import Wudgets from './components/Wudgets';
+const widgetprods=[
+  {name: 'Master Widget', price:'$125.00'},
+  {name: 'Sub Widget', price:'$115.00'},
+  {name: 'Long Widget', price:'$150.00'},
+  {name: 'Short Widget', price:'$135.00'}
+]
+const wudgetprods=[
+  {name: 'Wudget', price:'$75.00'},
+  {name: ' Wudget', price:'$85.00'},
+  {name: ' Wudget', price:'$135.00'},
+  {name: ' Wudget', price:'$110.00'}
+]
+const Application =()=>{
+  return (
+    <Router>
+      <div className='App'>
+        <Navbar />   
+          <div className='container'>
+            <Switch>
+              <Route exact path='/' component={Home} /> 
+              <Route
+                exact
+                path='/Widgets'
+                render={(props)=>
+                  <Fragment>
+                
+                    <Widgets prods={widgetprods}/>
+                  
+                  </Fragment>
+                }
+              />
+            <Route
+              exact
+              path='/Wudgets'
+              render={(props)=>
+                <Fragment>
+              
+                  <Wudgets prods={wudgetprods}/>
+                
+                </Fragment>
+              }
+            />
+            </Switch>
+</div>
+      </div>
+      </Router>
+  )};
+export default Application;
